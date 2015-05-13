fb2424fb68b37c2c5ca5fc32e7c3c53307c70639
xuelian1989 <365721358@qq.com>
2015-05-13 18:03:18 +0800

    设计 回复

--- Admin/Lib/Action/ShejiAction.class.php
+++ Admin/Lib/Action/ShejiAction.class.php
@@ -214,11 +214,12 @@ class ShejiAction extends CommonAction {
         else
             $this->error("操作失败！");
     }
+
     /**
      * 编辑案例
      */
-    public function edit_case(){
-        if(IS_POST){
+    public function edit_case() {
+        if (IS_POST) {
             $id = $_POST['id'];
             $title = trim($_POST['title']);
             $is_jd = $_POST['is_jd'];
@@ -290,25 +291,26 @@ class ShejiAction extends CommonAction {
             $data['addtime'] = time();
             $rs = $M->where("id=" . $id)->save($data);
             if ($rs)
-                $this->success("操作成功！",U('Sheji/index'));
+                $this->success("操作成功！", U('Sheji/index'));
             else
                 $this->error("操作失败！");
-            
+
             exit;
         }
         parent::_initalize();
-        $this->assign("systemConfig",$this->systemConfig);
-        $id=$_GET['id'];
-        if(empty($id)){
-            $this->error("请选择要编辑的案例");exit;
+        $this->assign("systemConfig", $this->systemConfig);
+        $id = $_GET['id'];
+        if (empty($id)) {
+            $this->error("请选择要编辑的案例");
+            exit;
         }
-        $M=M("Case");
-        $info=$M->where("id=".$id)->find();
-        $this->assign("info",$info);
-        
-        $this->assign("gzlist",$this->getgzh());#工长
-        $this->assign("hxcategory",$this->gethxlist());#户型
-        $this->assign("fgcategory",  $this->getfglist());#风格
+        $M = M("Case");
+        $info = $M->where("id=" . $id)->find();
+        $this->assign("info", $info);
+
+        $this->assign("gzlist", $this->getgzh()); #工长
+        $this->assign("hxcategory", $this->gethxlist()); #户型
+        $this->assign("fgcategory", $this->getfglist()); #风格
         $this->assign("tpjhlist", $this->gettpjhlist()); #图集分类
         if (!empty($info['img']))
             $imgsrc = json_decode($info['img']);
@@ -320,64 +322,65 @@ class ShejiAction extends CommonAction {
         $this->assign("imglist", $imglist);
         $this->display();
     }
-    
+
     /**
      * 添加幻灯片
      */
-    public function add_hdp(){
-        if(IS_POST){
-            $name=trim($_POST['name']);
-            $iminf=  $this->upload();
-            if(!empty($iminf[0]['savename'])){
-                $img="/Uploads/product/".$iminf[0]['savename'];
+    public function add_hdp() {
+        if (IS_POST) {
+            $name = trim($_POST['name']);
+            $iminf = $this->upload();
+            if (!empty($iminf[0]['savename'])) {
+                $img = "/Uploads/product/" . $iminf[0]['savename'];
             }
-            $link=trim($_POST['link']);
-            $uid=$_POST['gzid'];
-            $addtime=  time();
-            $status=$_POST['status'];
-            
-            $M=M("Hdpmember");
-            $data=array(
-                "name"=>$name,
-                "img"=>$img,
-                "link"=>$link,
-                "uid"=>$uid,
-                "addtime"=>$addtime,
-                "status"=>$status,
-                    "type"=>1
+            $link = trim($_POST['link']);
+            $uid = $_POST['gzid'];
+            $addtime = time();
+            $status = $_POST['status'];
+
+            $M = M("Hdpmember");
+            $data = array(
+                "name" => $name,
+                "img" => $img,
+                "link" => $link,
+                "uid" => $uid,
+                "addtime" => $addtime,
+                "status" => $status,
+                "type" => 1
             );
-            $rs=$M->add($data);
-            if($rs)
-                $this->success ("操作成功！");
+            $rs = $M->add($data);
+            if ($rs)
+                $this->success("操作成功！");
             else
-                $this->error ("操作失败！");
+                $this->error("操作失败！");
             exit;
         }
         parent::_initalize();
-        $this->assign("systemConfig",$this->systemConfig);
-        $this->assign("sjlist",$this->getgzh());
+        $this->assign("systemConfig", $this->systemConfig);
+        $this->assign("sjlist", $this->getgzh());
         $this->display();
     }
+
     /**
      * 幻灯片
      * 列表
      */
-    public function list_hdp(){
-        
+    public function list_hdp() {
+
         parent::_initalize();
         $this->assign("systemConfig", $this->systemConfig);
         $this->assign("gzlist", $this->getgzh()); #设计师
-        
+
         $keys = $_GET['keys'];
         $keys = $keys == "请输入关键字" ? "" : $keys;
         $uid = $_GET['uid'];
 
         $this->assign("keys", $keys);
         $this->assign("uid", $uid);
-        
+
         import("ORG.Util.Page");
         $where = "Hdp.type=1 ";
-        
+
         if (!empty($keys))
             $where.=" and Hdp.name like '%" . $keys . "%'";
         if (!empty($uid))
@@ -386,7 +389,7 @@ class ShejiAction extends CommonAction {
 
         $M = D("HdpView");
         $totalRows = $M->where($where)->count();
-        
+
         $p = new Page($totalRows, 10);
         $list = $M->where($where)->order("Hdp.addtime desc")->limit($p->firstRow . "," . $p->listRows)->select();
 
@@ -394,7 +397,7 @@ class ShejiAction extends CommonAction {
         $this->assign("page", $p->show());
         $inf = include_once './Common/config2.php';
         foreach ($list as $k => $v) {
-            
+
             $list[$k]['status_f'] = $v['status'] == 1 ? "已审核" : "未审核";
         }
         $this->assign("list", $list);
@@ -403,10 +406,11 @@ class ShejiAction extends CommonAction {
 
         $this->display();
     }
+
     /**
      * 删除幻灯片
      */
-    public function del_hpd(){
+    public function del_hpd() {
         $id = $_GET['id'];
         $M = M("Hdpmember");
         $rs = $M->where("id=" . $id)->delete();
@@ -415,25 +419,26 @@ class ShejiAction extends CommonAction {
         else
             $this->error("操作失败！");
     }
+
     /**
      * 编辑
      * 幻灯片
      */
-    public function edit_hpd(){
-        
-        if(IS_POST){
+    public function edit_hpd() {
+
+        if (IS_POST) {
             $id = $_POST['id'];
             $name = trim($_POST['name']);
             $gzid = $_POST['gzid'];
             $status = trim($_POST['status']);
             $link = $_POST['link'];
-            $imgi=  $this->upload();
-            if(!empty($imgi[0]['savename'])){
-                $img="/Uploads/product/".$imgi[0]['savename'];
+            $imgi = $this->upload();
+            if (!empty($imgi[0]['savename'])) {
+                $img = "/Uploads/product/" . $imgi[0]['savename'];
             }
-            
+
             $data = array();
-            
+
             $M = M("Hdpmember");
             $info1 = $M->where("id=" . $id)->find();
             if (!empty($img)) {
@@ -451,44 +456,155 @@ class ShejiAction extends CommonAction {
             $data['addtime'] = time();
             $rs = $M->where("id=" . $id)->save($data);
             if ($rs)
-                $this->success("操作成功！",U('Sheji/list_hdp'));
-            else{
-                
+                $this->success("操作成功！", U('Sheji/list_hdp'));
+            else {
+
                 $this->error("操作失败！");
             }
             exit;
         }
         parent::_initalize();
-        $this->assign("systemConfig",$this->systemConfig);
-        $id=$_GET['id'];
-        if(empty($id)){
-            $this->error("请选择要编辑的幻灯片");exit;
+        $this->assign("systemConfig", $this->systemConfig);
+        $id = $_GET['id'];
+        if (empty($id)) {
+            $this->error("请选择要编辑的幻灯片");
+            exit;
         }
-        $M=M("Hdpmember");
-        $info=$M->where("id=".$id)->find();
-        $this->assign("info",$info);
-        $gzlist=$this->getgzh();
-        $this->assign("sjlist",$gzlist);#工长
-        
+        $M = M("Hdpmember");
+        $info = $M->where("id=" . $id)->find();
+        $this->assign("info", $info);
+        $gzlist = $this->getgzh();
+        $this->assign("sjlist", $gzlist); #工长
+
         $this->display("add_hdp");
     }
+
     /**
      * 快速修改状态
      * 幻灯片
      */
-    public function status_hdp(){
-        $id=$_GET['id'];
-        $status=$_GET['status'];
-        if($status==0)
-            $statusf=1;
+    public function status_hdp() {
+        $id = $_GET['id'];
+        $status = $_GET['status'];
+        if ($status == 0)
+            $statusf = 1;
         else
-            $statusf=0;
-        $M=M("Hdpmember");
-        $rs=$M->where("id=".$id)->save(array("status"=>$statusf));
-        if($rs)
-            $this->success ("操作成功");
+            $statusf = 0;
+        $M = M("Hdpmember");
+        $rs = $M->where("id=" . $id)->save(array("status" => $statusf));
+        if ($rs)
+            $this->success("操作成功");
         else
-            $this->error ("操作失败！");
+            $this->error("操作失败！");
+    }
+
+    /**
+     * 口碑点评
+     */
+    public function koubeicomment() {
+        parent::_initalize();
+        $this->assign("systemConfig", $this->systemConfig);
+        import("ORG.Util.Page");
+        $where = "type=3";
+        $is_hf = $_GET['is_hf'];
+        $this->assign("is_hf", $is_hf);
+        $uid = $_GET['uid'];
+        $this->assign("uid", $uid);
+        if (!empty($is_hf)) {
+            $is_hf = $is_hf == 2 ? 0 : $is_hf;
+            $where .= " and is_hf=" . $is_hf;
+        }
+        if (!empty($uid))
+            $where .= " and sjuid=" . $uid;
+
+        $M = M("Koubeicomment");
+        $totalRows = $M->where($where)->count();
+        $p = new Page($totalRows, 10);
+        $list = $M->where($where)->order("addtime desc")->limit($p->firstRow . "," . $p->listRows)->select();
+        $arrhf = array("未回复", "已回复");
+        $arrs = array("未审核", "已审核");
+        foreach ($list as $k => $v) {
+            $list[$k]['is_hf_f'] = $arrhf[$v['is_hf']];
+            $list[$k]['status_f'] = $arrs[$v['status']];
+        }
+
+        $this->assign("list", $list);
+        $this->assign("page", $p->show());
+        $gzlist = $this->getgzh(); #获取商城
+        $this->assign("gzlist", $gzlist);
+        $this->display();
+    }
+
+    /**
+     * 口碑状态
+     */
+    public function status_koubei() {
+        $id = $_GET['id'];
+        $status = $_GET['status'];
+        if ($status == 1) {
+            $status_f = 0;
+        } else {
+            $status_f = 1;
+        }
+        $M = M("Koubeicomment");
+        $rs = $M->where("id=" . $id)->save(array("status" => $status_f));
+        if ($rs)
+            $this->success("操作成功！");
+        else
+            $this->error("操作失败！");
+    }
+
+    /**
+     * 删除口碑
+     */
+    public function del_koubei() {
+        $id = $_GET['id'];
+        $M = M("Koubeicomment");
+        $rs = $M->where("id=" . $id)->delete();
+        if ($rs)
+            $this->success("删除成功！");
+        else
+            $this->error("删除失败！");
+    }
+
+    /**
+     * 回复
+     */
+    public function hf() {
+        if (IS_POST) {
+            $id = $_POST['id'];
+            $hf_content = trim($_POST['hf_content']);
+            $M = M("Koubeicomment");
+            if (empty($hf_content)) {
+                $this->error("回复内容不能为空！");
+                exit;
+            }
+            $rs = $M->where("id=" . $id)->save(array("hf_content" => $hf_content, "hf_time" => time(), "is_hf" => 1));
+            if ($rs)
+                $this->success("操作成功！", U("Sheji/koubeicomment"));
+            else
+                $this->error("操作失败！");
+
+            exit;
+        }
+        parent::_initalize();
+        $this->assign("systemConfig", $this->systemConfig);
+        $M = M("Koubeicomment");
+        $id = $_GET['id'];
+        $info = $M->where("id=" . $id)->find();
+        $this->assign("info", $info);
+        $kh = $this->getkehu_ins($info['uid']);
+        $this->assign("khmem", $kh['a_name'] . "[" . $kh['truename'] . "]");
+        $this->assign("addtime", date("Y-m-d H:i:s", $info['addtime']));
+        $is_good = $info['is_good'] == 1 ? "好评" : "差评";
+        $this->assign("is_good_f", $is_good);
+        $sj = $this->getgzh_ins($info['sjuid']);
+        $this->assign("sjmem", $sj['a_name'] . "[" . $sj['truename'] . "]");
+        $status_f = $info['status'] == 1 ? "已审核" : "未审核";
+
+        $this->assign("status_f", $status_f);
+
+        $this->display();
     }
 
     //------------------------------------------------------------------
@@ -507,8 +623,18 @@ class ShejiAction extends CommonAction {
         }
         $M = M("Shejiview");
         $list = $M->where($where)->field("a_id,a_name,truename")->select();
-       
+
         return $list;
+    }
+
+    /**
+     * 获取工长详细
+     */
+    private function getgzh_ins($aid) {
+        $where = "a_id=" . $aid;
+        $M = M("Shejiview");
+        $info = $M->where($where)->field("a_id,a_name,truename")->find();
+        return $info;
     }
 
     /**
@@ -599,6 +725,16 @@ class ShejiAction extends CommonAction {
                 $data = array("status" => 0, "data" => "");
         }
         echo json_encode($data);
+    }
+
+    /**
+     * 获取客户详细
+     */
+    private function getkehu_ins($aid) {
+        $where = "a_id=" . $aid;
+        $M = M("Kehuview");
+        $info = $M->where($where)->field("a_id,a_name,truename")->find();
+        return $info;
     }
 
 }
